7faf5bb17381ec5ff3613cfa8f82fe6487252bef
Loan <loan.benedetto@gmail.com>
2025-09-19 12:27:49 +0200

    ajout de Utilisateur.borrow

--- Utilisateur.h
+++ Utilisateur.h
@@ -1,9 +1,18 @@
 #pragma once
 #include <string>
+#include <vector>
 
 class Mediatheque; // Decalration anticipée pour les circular import et eviter les pb
+class Ressource; // pas obligé mais safe
 
 class Utilisateur {
+
+protected:
+    int id;
+    std::string prenom;
+    std::string nom;
+
+    std::vector<int> emprunts; // par ID
 
 public:
     Utilisateur(int id, const std::string& prenom, const std::string& nom);
@@ -12,14 +21,10 @@ public:
     void list(const Mediatheque& mediatheque);
     void search(const std::string& q);
     void show(int id);
-    void borrow(int id);
+    void borrow(Mediatheque& mediatheque, int id);
 
     int getId() const { return id; }
     const std::string& getNom() const { return nom; }
     const std::string& getPrenom() const { return prenom; }
 
-protected:
-    int id;
-    std::string prenom;
-    std::string nom;
 };
