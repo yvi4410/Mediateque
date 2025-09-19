600af27d607718d3289203db8e6b68c0e605cf44
Loan <loan.benedetto@gmail.com>
2025-09-19 09:54:24 +0200

    Suppression de Application, ajout du lien Madiatheque-Ressources

--- Mediatheque.h
+++ Mediatheque.h
@@ -1,17 +1,24 @@
 #pragma once
-    #include <string>
+#include <string>
+#include <vector>
+#include <memory>
+#include "Ressource.h"
 
-    class Mediatheque {
-    public:
-        void dispatch();
-        void bye();
-        void add(const std::string& type);
-        void load(const std::string& filename);
-        void save(const std::string& filename);
-        void search(const std::string& chaine);
-        void clear();
-        void list();
-        void show(int id);
-        void deleteResource(int id);
-        void reset();
-    };
+class Mediatheque {
+
+private:
+    std::vector<std::unique_ptr<Ressource>> ressources;
+
+public:
+    void dispatch();
+    void bye();
+    void add(const std::string& type);
+    void load(const std::string& filename);
+    void save(const std::string& filename);
+    void search(const std::string& chaine);
+    void clear();
+    void list();
+    void show(int id);
+    void deleteResource(int id);
+    void reset();
+};
